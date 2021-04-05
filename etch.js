663a5022d2d537e4e63584189fc15e8fb1e7b80e
Kevin Mitchell <kmitch1096@gmail.com>
2021-04-05 17:30:05 -0400

    Initial Commit with files and ReadMe

---
+++ etch.js
@@ -0,0 +1,29 @@
+function sketch() {
+
+}
+
+var main = document.querySelector("#main")
+
+var first = document.createElement('div')
+var second = document.createElement('div')
+var third = document.createElement('div')
+var fourth = document.createElement('div')
+var fifth = document.createElement('div')
+var sixth = document.createElement('div')
+var seventh = document.createElement('div')
+var eighth = document.createElement('div')
+var ninth = document.createElement('div')
+var tenth = document.createElement('div')
+var eleventh = document.createElement('div')
+var twelfth = document.createElement('div')
+var thirteenth = document.createElement('div')
+var fourteenth = document.createElement('div')
+var fifteenth = document.createElement('div')
+var sixteenth = document.createElement('div')
+
+first.style.gridArea = 'one'
+first.addEventListener('hover', sketch)
+
+
+
+document.main.appendChild(first)
