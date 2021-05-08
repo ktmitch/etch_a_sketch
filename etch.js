a0404128c9166fd1249744fbb98a1d1ce594f5b3
Kevin Mitchell <kmitch1096@gmail.com>
2021-05-08 05:19:57 -0400

    Grid Layour now Working Successfully, Button logic still needs some editing along with the addition of styles

--- etch.js
+++ etch.js
@@ -3,51 +3,110 @@ var main = document.querySelector("#main")
 const allDivs = document.querySelectorAll("div")
 
 var randColor = Math.floor(Math.random() * 255)
+var color = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')'
 
+const newDiv = document.createElement('div')
 
+var i = 1
 
-function create() {
-    const newDiv = document.createElement('div')
-    newDiv.classList.add("allDivs")
-    newDiv.setAttribute('style', 'border: 1px black solid; width: 100%; height: 0; padding-bottom: 100%; margin: 0') //overflow hidden if adding content
-    newDiv.addEventListener('mouseenter', () => {
-        var randColor = Math.floor(Math.random() * 255)
-        newDiv.style.backgroundColor = 'lightblue'
-    })
-    main.appendChild(newDiv)
-    console.log("Added")
+function reset() {
+    main.removeChild(newDiv)
 }
 
-var i = 0
-
 window.addEventListener('load', () => {
-for (i ; i < 256; i++) {
     create()
-}})
+})
+
+function create() {
+    
+    var finalInt = 16
+    
+
+    var btn = document.querySelector("#button-clear")
+
+    document.documentElement.style.setProperty('--repeat', finalInt)
+
+    for (i; i <= finalInt * finalInt; i++) {
+
+        const newDiv = document.createElement('div')
+        newDiv.classList.add("allDivs")
+        newDiv.setAttribute('style', 'border: 1px black solid; width: 100%; height: 0; padding-bottom: 100%; margin: 0') //overflow hidden if adding content
+
+        newDiv.addEventListener('mouseenter', () => {
+            newDiv.style.backgroundColor = 'lightblue'
+            // newDiv.style.setProperty("backgroundColor", color)
+        })
+
+        main.appendChild(newDiv)
+        console.log("Added")
+        
+        
+        btn.addEventListener('click', () => {
+            main.removeChild(newDiv)
+        })
+        
+    }
+
+    btn.removeEventListener('click', reset)
+
+    }
+
+
+
 
 //////////////////////////////////////////////////
 
+function randomColor() {
+    newDiv.addEventListener('mouseenter', () => {
+        var randColor = Math.floor(Math.random() * 255)
+        newDiv.style.backgroundColor = randColor;
+})
+}
 
 function clear() {
 
+    // for
 
-    var allSquares = document.querySelectorAll(".allDivs")
+    // main.removeChild(newDiv)
 
-    for(var l = 0; l < allSquares.length; l++) {
-        allSquares[l].style.backgroundColor = 'white'
-    }
+
+    // var allSquares = document.querySelectorAll(".allDivs")
+
+    // for(var l = 0; l < allSquares.length; l++) {
+    //     allSquares[l].style.backgroundColor = 'white'
+    // }
 
     var answer = prompt("How Many Squares Per Side?")
     var answerInt = parseInt(answer)
     console.log(answerInt)
     
-    if (answerInt <= 100 && answerInt > 0) {
+    if (answerInt <= 50 && answerInt > 0) {
+
         var finalInt = answerInt * answerInt
         console.log(finalInt)
-        // for (x = 0; x < finalInt; x++) {
-        var finalGrid = document.documentElement.style.setProperty('--repeat', finalInt)
-        console.log(finalGrid)
-        create()
+
+
+        var btn = document.querySelector("#button-clear")
+        document.documentElement.style.setProperty('--repeat', answerInt)
+
+        for (i = 1; i <= finalInt; i++) {
+
+            const newDiv = document.createElement('div')
+            newDiv.classList.add("allDivs")
+            newDiv.setAttribute('style', 'border: 1px black solid; width: 100%; height: 0; padding-bottom: 100%; margin: 0') //overflow hidden if adding content
+
+            newDiv.addEventListener('mouseenter', () => {
+                newDiv.style.backgroundColor = 'lightblue'
+        })
+
+        main.appendChild(newDiv)
+        console.log("Selection Valid")
+
+        btn.addEventListener('click', () => {
+            main.removeChild(newDiv)
+        })
+
+    }
         // }
     }
     else {
@@ -55,6 +114,6 @@ function clear() {
     }
 }
 
-
+ 
 var btn = document.querySelector("#button-clear")
 btn.addEventListener('click', clear)
